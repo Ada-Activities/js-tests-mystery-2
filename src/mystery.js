8db18d78adf5903155fd64cd81759f76ca769a08
Ashley Yang <ashley.minghui@gmail.com>
2024-11-14 11:29:14 -0800

    Add solution for Mystery 2

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,11 @@
-export const mystery2 = (num) => {};
+export const mystery2 = (num) => {
+  // some of these could probably be combined
+  if (num % 4 === 0) {
+    if (num % 100 === 0) {
+      return !(num % 400);
+    }
+    return true;
+  }
+
+  return false;
+};
